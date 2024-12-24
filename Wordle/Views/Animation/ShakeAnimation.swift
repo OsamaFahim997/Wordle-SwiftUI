e4193e2b52131a436f1954ecd943db872472535e
Osama Fahim <osamafahim997@gmail.com>
2024-12-24 16:41:44 +0100

    Add shake animation.

---
+++ Wordle/Views/Animation/ShakeAnimation.swift
@@ -0,0 +1,20 @@
+//
+//  ShakeAnimation.swift
+//  Wordle
+//
+//  Created by Osama Fahim on 23/12/24.
+//
+
+import SwiftUI
+
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 10
+    var shakesPerUnit = 3
+    var animatableData: CGFloat
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+            amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+            y: 0))
+    }
+}
